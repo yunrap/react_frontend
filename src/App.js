9a5a3de98d06391000d55842277bfb921520d8c8
yunrap <dbsdpwl7@gmail.com>
2024-01-08 22:59:45 +0900

    라우터 적용 (게시판화면추가예정)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useEffect, useState } from "react";
+import Home from './home.js';
+import Test from './test.js';
+import { BrowserRouter, Routes } from "react-router-dom";
+import { useEffect } from "react";
 
 function App() {
-  const [message, setMessage] = useState("");
 
   useEffect(() => {
     fetch("/test")
       .then((res) => res.text())
-      .then((m) => setMessage(m));
   }, []);
 
   useEffect(() => {
@@ -30,20 +30,12 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App- header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p> {message} </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='App'>
+        <BrowserRouter>
+          <Routes path="/" component={Home} />
+          <Routes path="/test" component={Test} />
+        </BrowserRouter>
+      </div>
   );
 }
 
---
+++ src/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class home extends Component {
+
+    render() {
+        return (
+            <div>
+                <h3> hello </h3>
+            </div>
+        );
+    }
+}
+
+export default home;
