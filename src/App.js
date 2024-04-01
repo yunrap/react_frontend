6a30feaa8d2559da3e84c51745ff3ff1ca83fb79
yunrap <dbsdpwl7@gmail.com>
2024-04-01 22:57:20 +0900

    페이지 파일구조분리, 라우터적용

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
 import "./App.css";
-import Home from './home.js';
-import Test from './test.js';
-import { BrowserRouter, Routes } from "react-router-dom";
+import Home from "./home.js";
+import Test from "./pages/test.js";
+import { Route, Routes } from "react-router-dom";
 import { useEffect } from "react";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-
   useEffect(() => {
-    fetch("/test")
-      .then((res) => res.text())
+    fetch("/test").then((res) => res.text());
   }, []);
 
-  useEffect(() => {
-    fetch("/login", {
-      method: "post",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({
-        userName: "홍길동",
-        pwd: "1234",
-      }),
-    })
-      .then((res) => res.json())
-      .then((token) => {
-        localStorage.setItem("jwt", token.accessToken);
-        // alert("로그인 되었습니다");
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("/login", {
+  //     method: "post",
+  //     headers: {
+  //       "content-type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       userName: "홍길동",
+  //       pwd: "1234",
+  //     }),
+  //   })
+  //     .then((res) => res.json())
+  //     .then((token) => {
+  //       localStorage.setItem("jwt", token.accessToken);
+  //       alert("로그인 되었습니다");
+  //     });
+  // }, []);
 
   return (
-      <div className='App'>
-        <BrowserRouter>
-          <Routes path="/" component={Home} />
-          <Routes path="/test" component={Test} />
-        </BrowserRouter>
-      </div>
+    <div className="App">
+      <Routes>
+        <Route path="/" component={Home} />
+        <Route path="/test" component={Test} />
+        <Route path="/loginPage" element={<LoginPage />} />
+      </Routes>
+    </div>
   );
 }
 
