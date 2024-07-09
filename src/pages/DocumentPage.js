16fca3de98673790acb5132e78b1ebe4cc86d84c
yunrap <dbsdpwl7@gmail.com>
2024-07-09 11:56:40 +0900

    문서 화면조회 css,html 수정

--- src/pages/DocumentPage.js
+++ src/pages/DocumentPage.js
@@ -1,40 +1,116 @@
-import React, { Component } from "react";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const DocumentPage = () => {
-  const params = { userId: "ghost01", take: 10, skip: 0 };
+  const [inboxList, setInboxList] = useState([]);
+
+  const getInboxList = async () => {
+    const params = { userId: "ghost01", take: 10, skip: 0 };
+
+    const resp = await (
+      await axios.get(`/api/document/archive`, { params })
+    ).data;
+    setInboxList(resp.data);
+  };
 
   useEffect(() => {
-    axios
-      .get(`/api/document/archive`, { params })
-
-      .then((response) => console.log(response.data));
+    getInboxList();
   }, []);
 
   return (
-    <main>
-      <section class="document-section">
-        <h2>나의 문서 목록</h2>
-        <ul class="document-list">
-          <li class="document-item">
-            <a href="document1.pdf" target="_blank">
-              INBOX
-            </a>
-          </li>
-          <li class="document-item">
-            <a href="document2.pdf" target="_blank">
-              OUTBOX
-            </a>
-          </li>
-          <li class="document-item">
-            <a href="document3.pdf" target="_blank">
-              ARCHIVE
-            </a>
-          </li>
-        </ul>
-      </section>
-    </main>
+    <>
+      <div class="title">나의 문서 목록</div>
+      <div class="container">
+        <div class="container">
+          <div class="table-container">
+            <table>
+              <thead>
+                <tr>
+                  <th colspan="5">INBOX - 내가 결제해야할 문서</th>
+                </tr>
+                <tr>
+                  <th>번호</th>
+                  <th>제목</th>
+                  <th>내용</th>
+                  <th>사용자</th>
+                  <th>승인여부</th>
+                </tr>
+              </thead>
+              <tbody>
+                {inboxList.map((data) => (
+                  <>
+                    <tr key={data.idx}>
+                      <td>{data.documentNumber}</td>
+                      <td>{data.title}</td>
+                      <td>{data.content}</td>
+                      <td>{data.userId}</td>
+                      <td>{data.completeYn}</td>
+                    </tr>
+                  </>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div class="table-container">
+            <table>
+              <thead>
+                <tr>
+                  <th colspan="5">OUTBOX - 내가 생성한 문서(결제진행중)</th>
+                </tr>
+                <tr>
+                  <th>번호</th>
+                  <th>제목</th>
+                  <th>내용</th>
+                  <th>사용자</th>
+                  <th>승인여부</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Item 2A</td>
+                  <td>Item 2B</td>
+                  <td>Item 2C</td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="table-container">
+            <table>
+              <thead>
+                <tr>
+                  <th colspan="5">ARCHIVE - 내가 관여한문서(완료)</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Item 3A</td>
+                  <td>Item 3B</td>
+                  <td>Item 3C</td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 export default DocumentPage;
